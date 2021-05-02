a5361e96d9d7372c9811eb959b0cdf6ad0ba8f4c
Vitalifront <vitaliklopau@gmail.com>
2021-05-02 17:21:07 +0200

    Class INIT

--- script.js
+++ script.js
@@ -1,77 +1,21 @@
-//const title = document.querySelector("#title");
-/*title.innerHTML = "Hi from Js";
-title.style.color = "red";
-console.dir(document);*/
-
-/*function clickHandler() {
-
-    title.style.color = "red";
-}
-
-//title.addEventListener("click", clickHandler);
-
-
-
-*/
-
-
-/*if ("10" === 10) {
-    console.log("Hi");
-} else if ("10" === "10") {
-    console.log("DADADAD");
-} else {
-    console.log("pokapoka");
-}*/
-
-/*if (20 < 5 || "Anton" === "Anton") {
-    console.log("da");
-} else {
-    console.log("nonono");
-}
-*/
-
-/*const age = prompt("Napishite swoj wozrast");
-
-if (age >= 18 && age < 21) {
-    console.log("u can by");
-} else if (age > 21) {
-    console.log("go buhat");
-}
-else {
-    console.log(" go sleep");
-}
-*/
 const title = document.querySelector("#title");
-const BASE_COLOUR = "rgb(118, 218, 25)";
-const OTHER_COLOUR = "#8e44ad";
+const CLICKED_CLASS = "clicked";
 
 function clickHandler() {
-    const currentColour = title.style.color;
-    if (currentColour === BASE_COLOUR) {
-        title.style.color = OTHER_COLOUR;
+    const currentClass = title.className;
+    console.log(currentClass);
+    if (currentClass !== CLICKED_CLASS) {
+        title.className = CLICKED_CLASS;
     } else {
-        title.style.color = BASE_COLOUR;
+        title.className = "";
     }
-
 }
 
 function init() {
-    title.style.color = BASE_COLOUR;
     title.addEventListener("click", clickHandler);
 }
 
 init();
-
-function offlineHandler() {
-    console.log("bybyby");
-}
-function onlineHandler() {
-    console.log("hihihi");
-}
-
-
-window.addEventListener("offline", offlineHandler);
-window.addEventListener("online", onlineHandler);
 
 
 
