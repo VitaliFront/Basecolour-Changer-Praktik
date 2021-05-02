6f454c89d024ff2f05339ffd957739caad223b88
Vitalifront <vitaliklopau@gmail.com>
2021-05-02 17:40:15 +0200

    toggle !!!!!

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function clickHandler() {
-    const hasClicked = title.classList.contains(CLICKED_CLASS);
+    title.classList.toggle(CLICKED_CLASS);
+    /*const hasClicked = title.classList.contains(CLICKED_CLASS);
 
     if (!hasClicked) {
 
         title.classList.add(CLICKED_CLASS);
     } else {
         title.classList.remove(CLICKED_CLASS);
-    }
+    }*/
 }
 
 function init() {
